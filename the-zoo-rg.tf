3c4ec16f625e71361cd4a6c8a7a3a85672219d34
Pascal <pascalw@u.washington.edu>
2019-10-10 14:30:32 +0200

    Remove randomize for storage account name

--- the-zoo-rg.tf
+++ the-zoo-rg.tf
@@ -117,19 +117,9 @@ resource "azurerm_network_interface" "ubuntu-vm-1-nic" {
     }
 }
 
-# Generate random text for a unique storage account name
-resource "random_id" "randomId" {
-    keepers = {
-        # Generate a new ID only when a new resource group is defined
-        resource_group = "${azurerm_resource_group.the-zoo-rg.name}"
-    }
-    
-    byte_length = 8
-}
-
-# Create storage account for boot diagnostics
+# Create storage account
 resource "azurerm_storage_account" "storage" {
-    name                        = "diag${random_id.randomId.hex}"
+    name                        = "storage"
     resource_group_name         = "${azurerm_resource_group.the-zoo-rg.name}"
     location                    = "northeurope"
     account_tier                = "Standard"
