f2eb84aaeacbf6b83bcdfe69e0d27707cabba729
Pascal <pascalw@u.washington.edu>
2019-10-13 17:10:21 +0200

    Rename all occurences of the-zoo

--- terraform-plans/main.tf
+++ terraform-plans/main.tf
@@ -3,8 +3,8 @@ provider "azurerm" {
 }
 
 # Create a resource group if it doesn’t exist
-resource "azurerm_resource_group" "the-zoo-rg" {
-    name     = "the-zoo-rg"
+resource "azurerm_resource_group" "gitops-zoo" {
+    name     = "gitops-zoo"
     location = "northeurope"
 
     tags = {
@@ -17,7 +17,7 @@ resource "azurerm_virtual_network" "vnet" {
     name                = "vnet"
     address_space       = ["10.0.0.0/16"]
     location            = "northeurope"
-    resource_group_name = "${azurerm_resource_group.the-zoo-rg.name}"
+    resource_group_name = "${azurerm_resource_group.gitops-zoo.name}"
 
     tags = {
         cdsid = "pwalleni"
@@ -27,7 +27,7 @@ resource "azurerm_virtual_network" "vnet" {
 # Create subnet
 resource "azurerm_subnet" "subnet" {
     name                 = "subnet"
-    resource_group_name  = "${azurerm_resource_group.the-zoo-rg.name}"
+    resource_group_name  = "${azurerm_resource_group.gitops-zoo.name}"
     virtual_network_name = "${azurerm_virtual_network.vnet.name}"
     address_prefix       = "10.0.1.0/24"
 }
@@ -36,7 +36,7 @@ resource "azurerm_subnet" "subnet" {
 resource "azurerm_public_ip" "ubuntu-vm-0-publicip" {
     name                         = "ubuntu-vm-0-publicip"
     location                     = "northeurope"
-    resource_group_name          = "${azurerm_resource_group.the-zoo-rg.name}"
+    resource_group_name          = "${azurerm_resource_group.gitops-zoo.name}"
     allocation_method            = "Dynamic"
 
     tags = {
@@ -48,7 +48,7 @@ resource "azurerm_public_ip" "ubuntu-vm-0-publicip" {
 resource "azurerm_public_ip" "ubuntu-vm-1-publicip" {
     name                         = "ubuntu-vm-1-publicip"
     location                     = "northeurope"
-    resource_group_name          = "${azurerm_resource_group.the-zoo-rg.name}"
+    resource_group_name          = "${azurerm_resource_group.gitops-zoo.name}"
     allocation_method            = "Dynamic"
 
     tags = {
@@ -60,7 +60,7 @@ resource "azurerm_public_ip" "ubuntu-vm-1-publicip" {
 resource "azurerm_network_security_group" "nsg" {
     name                = "nsg"
     location            = "northeurope"
-    resource_group_name = "${azurerm_resource_group.the-zoo-rg.name}"
+    resource_group_name = "${azurerm_resource_group.gitops-zoo.name}"
     
     security_rule {
         name                       = "SSH"
@@ -83,7 +83,7 @@ resource "azurerm_network_security_group" "nsg" {
 resource "azurerm_network_interface" "ubuntu-vm-0-nic" {
     name                      = "ubuntu-vm-0-nic"
     location                  = "northeurope"
-    resource_group_name       = "${azurerm_resource_group.the-zoo-rg.name}"
+    resource_group_name       = "${azurerm_resource_group.gitops-zoo.name}"
     network_security_group_id = "${azurerm_network_security_group.nsg.id}"
 
     ip_configuration {
@@ -102,7 +102,7 @@ resource "azurerm_network_interface" "ubuntu-vm-0-nic" {
 resource "azurerm_network_interface" "ubuntu-vm-1-nic" {
     name                      = "ubuntu-vm-1-nic"
     location                  = "northeurope"
-    resource_group_name       = "${azurerm_resource_group.the-zoo-rg.name}"
+    resource_group_name       = "${azurerm_resource_group.gitops-zoo.name}"
     network_security_group_id = "${azurerm_network_security_group.nsg.id}"
 
     ip_configuration {
@@ -120,7 +120,7 @@ resource "azurerm_network_interface" "ubuntu-vm-1-nic" {
 # Create storage account
 resource "azurerm_storage_account" "thezoostorage" {
     name                        = "thezoostorage"
-    resource_group_name         = "${azurerm_resource_group.the-zoo-rg.name}"
+    resource_group_name         = "${azurerm_resource_group.gitops-zoo.name}"
     location                    = "northeurope"
     account_tier                = "Standard"
     account_replication_type    = "LRS"
@@ -134,7 +134,7 @@ resource "azurerm_storage_account" "thezoostorage" {
 resource "azurerm_virtual_machine" "ubuntu-vm-0" {
     name                  = "ubuntu-vm-0"
     location              = "northeurope"
-    resource_group_name   = "${azurerm_resource_group.the-zoo-rg.name}"
+    resource_group_name   = "${azurerm_resource_group.gitops-zoo.name}"
     network_interface_ids = ["${azurerm_network_interface.ubuntu-vm-0-nic.id}"]
     vm_size               = "Standard_DS1_v2"
 
@@ -175,7 +175,7 @@ resource "azurerm_virtual_machine" "ubuntu-vm-0" {
 resource "azurerm_virtual_machine" "ubuntu-vm-1" {
     name                  = "ubuntu-vm-1"
     location              = "northeurope"
-    resource_group_name   = "${azurerm_resource_group.the-zoo-rg.name}"
+    resource_group_name   = "${azurerm_resource_group.gitops-zoo.name}"
     network_interface_ids = ["${azurerm_network_interface.ubuntu-vm-1-nic.id}"]
     vm_size               = "Standard_DS1_v2"
 
